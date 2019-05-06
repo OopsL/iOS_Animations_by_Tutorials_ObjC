ff1c28006899d2de4efe6fc4959cd39f1c774e04
wukai <wukai@brilliance.com.cn>
2019-05-06 17:56:17 +0800

    add

--- Bahama/Chapter_1_3_Bahama/ViewController.m
+++ Bahama/Chapter_1_3_Bahama/ViewController.m
@@ -194,6 +194,8 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
 }
 
 @end
